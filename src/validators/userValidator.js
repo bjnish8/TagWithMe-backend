2503e71fdd9434b0fa0705da43cf74b2c6e5b88b
Binish Koirala <binishk@bgsu.edu>
2019-09-21 03:05:06 -0400

    Created a validator for users

---
+++ src/validators/userValidator.js
@@ -0,0 +1,54 @@
+const User = require('../models/user');
+
+const validate = {
+    async registration(req, res, next) {
+        let email_regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+        // Check if none of the required fields are empty
+        if (!req.body.email || !req.body.password1 || !req.body.password2 || !req.body.first_name || !req.body.last_name) {
+            return res.status(400).json(({
+                error: {
+                    message: "Please fill out all the fields"
+                }
+            }))
+        }
+        // Check if password and verify_password fields match
+        else if (req.body.password1 !== req.body.password2) {
+            return res.status(400).json({
+                error: {
+                    message: "Passwords do not match"
+                }
+            })
+        }
+
+        // Use regex to check if email provided is a valid email
+        else if (!email_regex.test(req.body.email.toLowerCase())) {
+            return res.status(400).json({
+                error: {
+                    message: "Please provide a valid email address"
+                }
+            })
+        }
+
+        else {
+        // Last, check if the email pre-exists in the database
+            const user = await User.find({
+                email: req.body.email
+            }).exec()
+
+            if (user.length === 0) {             // If no users exist already
+                next();
+            } else {
+                return res.status(400).json({
+                    error: {
+                        message: "User with that email address already exists. Please provide a different email"
+                    }
+                })
+            }
+        }
+    }
+}
+
+module.exports = {
+    validate
+}
